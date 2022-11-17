e7ae4c86e2b0c578461f9002c7dda9f1239be73e
JesseThao <qx5471dg@go.minnstate.edu>
2022-11-17 16:16:38 -0600

    updated admin class and added two new classes, still WIP

---
+++ Computer_Management_System/src/main/java/controller/AdminGetProduct.java
@@ -0,0 +1,56 @@
+package controller;
+
+import java.io.IOException;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import model.Product;
+
+public class AdminGetProduct {
+	
+	private static final long serialVersionUID = 1L;
+	private Connection connection;
+    
+	public void init() {
+		try {
+			Class.forName("com.mysql.cj.jdbc.Driver");
+			connection = DriverManager.getConnection("jdbc:mysql://cmhs.cdrxbvksu13u.us-east-1.rds.amazonaws.com/cmhs", "admin", "IW2Radlf!");
+		} catch (SQLException e) {
+			e.printStackTrace();
+		} catch (ClassNotFoundException e) {
+			e.printStackTrace();
+		}
+	}
+       
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		try {
+			HttpSession session = request.getSession();
+			Statement statement = connection.createStatement();		
+			String productID = request.getParameter("prodID");
+			Product product = new Product();
+			ResultSet rs = statement.executeQuery("SELECT * FROM cmhs.products WHERE productID = '" + productID + "'");
+			product = product.getProduct(rs);
+			
+		}
+		
+		catch (SQLException e) {
+			e.printStackTrace();
+		}
+	}
+	
+	public void destroy(){
+		try {
+			connection.close();
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+	}
+}
